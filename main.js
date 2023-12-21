280ca2f3d44135952a2e2b336667bf2b4cb859f7
Sobur Abubakar <abubakarsobur440@gmail.com>
2023-12-21 14:39:16 +0100

    update

--- main.js
+++ main.js
@@ -1,2 +1,66 @@
 import "./style.css"
 
+
+//Preloader
+
+const preloader = () => {
+  const counter = document.querySelector(".counter")
+  let count = 0
+  
+  const updateCounter = () => {
+      count += Math.floor(Math.random() * 10) + 1
+      if(count >= 100) {
+          count = Math.min(count, 100)
+      }
+
+      counter.textContent = count + "%"
+
+  }
+  
+  setTimeout(() => {
+      setInterval(updateCounter, 350)
+  }, 400)
+}
+preloader()
+
+const tl = gsap.timeline()
+
+tl.to(".loader", {
+  delay: 7.5,
+  opacity: 0
+})
+.set(".loader", {
+  display: "none"
+})
+.to("#content", {
+  y: 0,
+  opacity: 1,
+  duration: 1,
+  ease: "power3.out"
+})
+
+const imgObserver = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if(entry.isIntersecting) {
+      gsap.timeline()
+      .to(".image1, .image2, .image3", {
+        clipPath: "polygon(0 100%, 100% 100%, 100% 0, 0 0)",
+        stagger: {amount: 0.5}
+      })
+      .to(".source", {
+        opacity: 1
+      })
+      .to(".checks__container", {
+        opacity: 1,
+        duration: 1,
+        ease: "power3.out"
+      })
+      .to(".footer", {
+        opacity: 1,
+        duration: 1,
+        ease: "power3.out"
+      }, "<")
+    }
+  })
+}, {threshold: 1})
+imgObserver.observe(document.querySelector(".images__container"))
